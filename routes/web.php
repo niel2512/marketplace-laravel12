53c4c43d20565ade76b9b09523923a9216459779
niel2512 <nielyusuf17@gmail.com>
2025-04-18 00:52:21 +0700

    Konfigurasi Dashboard SB Admin Bootstrap

--- routes/web.php
+++ routes/web.php
@@ -6,12 +6,18 @@ Route::get('/', function () {
     return redirect('/login');
 });
 
+Route::get('/dashboard', function () {
+    return view('dashboardbootstrap');
+})->middleware(['auth'])->name('dashboardbootstrap');
+
+
+
 Route::middleware([
     'auth:sanctum',
     config('jetstream.auth_session'),
     'verified',
-])->group(function () {
-    Route::get('/dashboard', function () {
-        return view('dashboard');
-    })->name('dashboard');
-});
+// ])->group(function () {
+//     Route::get('/dashboard', function () {
+//         return view('dashboard');
+//     })->name('dashboard');
+]);
